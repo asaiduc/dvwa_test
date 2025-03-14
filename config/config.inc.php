fff93fc464d3691fda376b078e0c27a21b34ab77
asaiduc <128478575+asaiduc@users.noreply.github.com>
2025-03-14 21:36:11 +0700

    Update config.inc.php

--- config/config.inc.php
+++ config/config.inc.php
@@ -15,10 +15,10 @@ $DBMS = 'MySQL';
 # If you are using MariaDB then you cannot use root, you must use create a dedicated DVWA user.
 #   See README.md for more information on this.
 $_DVWA = array();
-$_DVWA[ 'db_server' ]   = getenv('DB_SERVER') ?: '127.0.0.1';
+$_DVWA[ 'db_server' ]   = 'dbdvwa.mysql.database.azure.com';
 $_DVWA[ 'db_database' ] = 'dvwa';
-$_DVWA[ 'db_user' ]     = 'dvwa';
-$_DVWA[ 'db_password' ] = 'p@ssw0rd';
+$_DVWA[ 'db_user' ]     = 'binhduc';
+$_DVWA[ 'db_password' ] = 'Asaiduc@1910';
 $_DVWA[ 'db_port']      = '3306';
 
 # ReCAPTCHA settings
